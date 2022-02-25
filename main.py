fb33335c5a05d805a9dbf42f986010347a2bb55c
25ChilingH <25ChilingH@students.harker.org>
2022-02-24 21:56:52 -0800

    Optimized model accuracy

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
+# %% [code] {"execution":{"iopub.status.busy":"2022-02-24T02:19:03.222374Z","iopub.execute_input":"2022-02-24T02:19:03.22278Z","iopub.status.idle":"2022-02-24T02:19:06.078921Z","shell.execute_reply.started":"2022-02-24T02:19:03.222679Z","shell.execute_reply":"2022-02-24T02:19:06.077817Z"}}
 # helper libraries
 import numpy as np
 import pandas as pd
-from time import strftime
-import itertools
-import random
 import os
-from os import walk
-from os.path import join
-
-# data augmentation
-import albumentations as A
 
 # plotting
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 import matplotlib.image as mpimg
 import cv2
 import seaborn as sns
@@ -23,199 +16,98 @@ from sklearn.model_selection import train_test_split
 
 # model building
 import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import Dense, Activation, BatchNormalization, Flatten, Dropout
-from sklearn.utils import class_weight
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import MaxPooling2D, Dense, Dropout,Flatten, Conv2D
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import ReduceLROnPlateau
 
 # model evaluation
-from sklearn.model_selection import KFold
+import time
 
-METADATA_CSV = "../brain-tumor-dataset/metadata.csv"
-TUMOR = '../brain-tumor-dataset/Brain Tumor Data Set/Brain Tumor Data Set/Brain Tumor/'
-CONTROL = "../brain-tumor-dataset/Brain Tumor Data Set/Brain Tumor Data Set/Healthy/"
+# %% [code] {"execution":{"iopub.status.busy":"2022-02-24T02:19:11.22876Z","iopub.execute_input":"2022-02-24T02:19:11.229065Z","iopub.status.idle":"2022-02-24T02:19:12.043279Z","shell.execute_reply.started":"2022-02-24T02:19:11.229034Z","shell.execute_reply":"2022-02-24T02:19:12.042254Z"}}
+tumor_dir = '../input/brian-tumor-dataset/Brain Tumor Data Set/Brain Tumor Data Set/Brain Tumor'
+control_dir = '../input/brian-tumor-dataset/Brain Tumor Data Set/Brain Tumor Data Set/Healthy'
+filepaths = []
+targets = []
+for root, _, filenames in os.walk(control_dir):
+    for fName in filenames:
+        file_path = os.path.join(root, fName)
+        filepaths.append(file_path)
+        targets.append("control")
 
-def augment_data(image):
-    transform = A.Compose([
-        A.RandomRotate90(),
-        A.Flip(),
-        A.Transpose(),
-        A.OneOf([
-            A.MotionBlur(p=.2),
-            A.MedianBlur(blur_limit=3, p=0.3),
-            A.Blur(blur_limit=3, p=0.1),
-        ], p=0.2),
-        A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.2),
-        A.OneOf([
-            A.OpticalDistortion(p=0.3),
-            A.GridDistortion(p=.1),
-        ], p=0.2),
-        A.RandomBrightnessContrast(),            
-    ])
-    transform2 = A.Compose([
-        A.RandomRotate90(),
-        A.Flip(),
-        A.Transpose(),
-        A.GaussNoise(),
-        A.OneOf([
-            A.MotionBlur(p=.2),
-            A.MedianBlur(blur_limit=3, p=0.1),
-            A.Blur(blur_limit=3, p=0.1),
-        ], p=0.2),
-        A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.2),
-        A.OneOf([
-            A.OpticalDistortion(p=0.3),
-            A.GridDistortion(p=.1),
-            A.PiecewiseAffine(p=0.3),
-        ], p=0.2),
-        A.OneOf([
-            A.Sharpen(),
-            A.Emboss(),
-            A.RandomBrightnessContrast(),            
-        ], p=0.3),
-    ])
-    random.seed(42)
-    return [transform(image=image)['image'], transform2(image=image)['image']]
-    
 
-# the dataset has both grayscale and RGB images so we need to convert RGB images to single channels
-starting_data = pd.read_csv(METADATA_CSV)
-def rgb2gray(img):
-    output_img = img
-    if img.ndim == 3:
-        R, G, B = img[:, :, 0], img[:, :, 1], img[:, :, 2]
-        output_img = 0.2989 * R + 0.5870 * G + 0.1140 * B # converting the channels depending on diff weights 
-    if output_img.max() < 1.1:
-        output_img = output_img * 255
-    return output_img
+for root, _, filenames in os.walk(tumor_dir):
+    for fName in filenames:
+        file_path = os.path.join(root, fName)
+        filepaths.append(file_path)
+        targets.append("tumor")
 
-def reduce_noise(noisy_img):
-    clean_img = np.array(noisy_img)
-    clean_img[noisy_img < 10] = 0 # reduces negligible parts to 0
-    return clean_img
+series = pd.Series(filepaths, name="filepaths")
+series2 = pd.Series(targets, name="targets")
+tumor_df = pd.concat([series, series2], axis=1)
+df = pd.DataFrame(tumor_df)
 
-# some of the data have white white borders
-def remove_white_borders(img_with_borders):
-    # left
-    if np.mean(img_with_borders[:, 0]) > 30:
-        img_with_borders[:, 0] = 0
-    # right
-    if np.mean(img_with_borders[:, -1]) > 30:
-        img_with_borders[:, -1] = 0
-    # top
-    if np.mean(img_with_borders[0, :]) > 30:
-        img_with_borders[0, :] = 0
-    # bottom
-    if np.mean(img_with_borders[-1, :]) > 30:
-        img_with_borders[-1, :] = 0
-    return img_with_borders
+# %% [code] {"execution":{"iopub.status.busy":"2022-02-24T02:19:15.315225Z","iopub.execute_input":"2022-02-24T02:19:15.315608Z","iopub.status.idle":"2022-02-24T02:19:15.336333Z","shell.execute_reply.started":"2022-02-24T02:19:15.315559Z","shell.execute_reply":"2022-02-24T02:19:15.335566Z"}}
+_, testDf = train_test_split(df, test_size=0.3, random_state=42, shuffle= True)
+trainDf, valDf = train_test_split(df, test_size=0.2, random_state=42, shuffle=True)
 
-# all part of data normalization
-def remove_black_padding(img_with_padding):
-    # Sides
-    vert_mean = np.mean(img_with_padding, axis=0)
-    vert_map = vert_mean > 1
-    vert_matches = np.where(vert_map == True)
-    img_no_padding = img_with_padding[:, vert_matches[0][0]:vert_matches[0][-1]]
-    # Top & bottom
-    hori_mean = np.mean(img_no_padding, axis=1)
-    hori_map = hori_mean > 1
-    hori_matches = np.where(hori_map == True)
-    img_no_padding = img_no_padding[hori_matches[0][0]:hori_matches[0][-1], :]
-    return img_no_padding
+# %% [code] {"execution":{"iopub.status.busy":"2022-02-24T02:19:25.187529Z","iopub.execute_input":"2022-02-24T02:19:25.187906Z","iopub.status.idle":"2022-02-24T02:19:26.00691Z","shell.execute_reply.started":"2022-02-24T02:19:25.187866Z","shell.execute_reply":"2022-02-24T02:19:26.004316Z"}}
+gen = ImageDataGenerator(
+                            horizontal_flip=True,
+                            preprocessing_function= tf.keras.applications.mobilenet_v2.preprocess_input
+                        )
+# scaled between [-1, 1]
+train = gen.flow_from_dataframe(dataframe = trainDf, x_col="filepaths",y_col="targets",
+                                      target_size=(244,244),
+                                      color_mode='rgb',
+                                      class_mode="categorical",
+                                      batch_size=32,
+                                      shuffle=False
+                                     )
 
-def square_image(rect_img):
-    X, Y = rect_img.shape
-    if X != Y:
-        if X > Y:
-            out_img = np.zeros((X, X))
-            offset = int(np.ceil((X-Y) / 2))
-            out_img[:, offset : (offset + Y)] = rect_img
-        else:
-            out_img = np.zeros((Y, Y))
-            offset = int(np.ceil((Y-X) / 2))
-            out_img[offset : (offset + X), :] = rect_img
-        return out_img
-    else:
-        return rect_img
+val = gen.flow_from_dataframe(dataframe= valDf,x_col="filepaths", y_col="targets",
+                                    target_size=(244,244),
+                                    color_mode= 'rgb',
+                                    class_mode="categorical",
+                                    batch_size=32,
+                                    shuffle=False
+                                   )
 
-def preprocess_img(input_img):
-    output_img = rgb2gray(input_img)
-    output_img = reduce_noise(output_img)
-    output_img = remove_white_borders(output_img)
-    output_img = remove_black_padding(output_img)
-    output_img = square_image(output_img)
-    output_img = cv2.resize(output_img, dsize=(64, 64), interpolation=cv2.INTER_LINEAR)
-    return output_img
+testGen = ImageDataGenerator(preprocessing_function= tf.keras.applications.mobilenet_v2.preprocess_input)
+test = testGen.flow_from_dataframe(dataframe = testDf, x_col="filepaths", y_col="targets",
+                                     target_size=(244,244),
+                                     color_mode='rgb',
+                                     class_mode="categorical",
+                                     batch_size=32,
+                                     shuffle= False
+                                    )
 
-features = np.zeros((1, 4096))
-target = np.array([])
-debug=1
-# Fill with tumor images
-for root, dirnames, filenames in walk(TUMOR):
-    n_total = len(filenames)
-    n = 1
-    for filename in filenames:
-        file_path = join(root, filename)
-        n += 1       
-        img = mpimg.imread(file_path)
-        if n % 50 == 0:
-            imgs = augment_data(img)
-            for image in imgs:
-                img = preprocess_img(image)
-                features = np.vstack([features, np.reshape(img, -1)])
-                target = np.append(target, 1)
-            n+=2
-            print(file_path)
-        img = preprocess_img(img)
-        features = np.vstack([features, np.reshape(img, -1)])
-        target = np.append(target, 1)
-features = features[1:, :]
-
-for root, dirnames, filenames in walk(CONTROL):
-    n_total = len(filenames)
-    n = 1
-    for filename in filenames:
-        file_path = join(root, filename)
-        n += 1       
-        img = mpimg.imread(file_path)
-        if n % 50 == 0:
-            imgs = augment_data(img)
-            for image in imgs:
-                img = preprocess_img(image)
-                features = np.vstack([features, np.reshape(img, -1)])
-                target = np.append(target, 0)
-            n+=2
-            print(file_path)
-        img = preprocess_img(img)
-        features = np.vstack([features, np.reshape(img, -1)])
-        target = np.append(target, 0)
-        
-df = pd.DataFrame(features)
-df["target"] = target
-
-target = df["target"]
-features = df.drop("target", axis=1)
-x_train, x_test, y_train, y_test = train_test_split(features, target, test_size=0.1, random_state=42, stratify=target)
-
+# %% [code] {"execution":{"iopub.status.busy":"2022-02-24T02:19:28.076844Z","iopub.execute_input":"2022-02-24T02:19:28.077172Z","iopub.status.idle":"2022-02-24T02:19:28.464508Z","shell.execute_reply.started":"2022-02-24T02:19:28.077131Z","shell.execute_reply":"2022-02-24T02:19:28.463002Z"}}
 model = Sequential()
-
-model.add(BatchNormalization())
-model.add(Dense(256, input_dim=4096, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dense(128, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(16, activation='relu'))
-model.add(BatchNormalization())
+model.add(Conv2D(filters=32,kernel_size=(3,3),strides=(1,1), activation="relu", padding="valid",
+               input_shape=(244,244,3)))
+model.add(MaxPooling2D(strides=2))
+model.add(Conv2D(filters=48, kernel_size=(5,5), padding='valid', activation='relu'))
+model.add(MaxPooling2D(strides=2))
+model.add(Flatten())
+model.add(Dropout(0.5))
+model.add(Dense(256, activation='relu'))
+model.add(Dense(84, activation='relu'))
 model.add(Dense(2, activation='softmax'))
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
 
-history = model.fit(x_train, y_train, epochs=70, batch_size=100, validation_split=0.13, shuffle=True)
-scores = model.evaluate(x_test, y_test)
+# %% [code] {"execution":{"iopub.status.busy":"2022-02-24T02:19:30.718223Z","iopub.execute_input":"2022-02-24T02:19:30.718566Z","iopub.status.idle":"2022-02-24T02:52:37.328071Z","shell.execute_reply.started":"2022-02-24T02:19:30.718527Z","shell.execute_reply":"2022-02-24T02:52:37.326993Z"}}
+start = time.time()
+rlr = ReduceLROnPlateau(monitor='val_accuracy', verbose=1, mode='max',factor=0.3, min_lr=1e-6, patience=2)
 
+history = model.fit(train, epochs=10, validation_data=val, verbose=1, callbacks=[rlr])
+print("Total time: ", time.time() - start, "seconds")
+model.evaluate(test)
 
+# %% [code] {"execution":{"iopub.status.busy":"2022-02-24T02:53:03.585872Z","iopub.execute_input":"2022-02-24T02:53:03.586167Z","iopub.status.idle":"2022-02-24T02:53:03.594557Z","shell.execute_reply.started":"2022-02-24T02:53:03.586138Z","shell.execute_reply":"2022-02-24T02:53:03.59375Z"}}
 def plotHist(histories):
     plt.plot(histories.history['accuracy'])
     plt.plot(histories.history['val_accuracy'])
@@ -233,20 +125,32 @@ def plotHist(histories):
     plt.legend(['train', 'val'], loc='upper left')
     plt.show()
 
-model.summary()
+# %% [code] {"execution":{"iopub.status.busy":"2022-02-24T02:53:04.41939Z","iopub.execute_input":"2022-02-24T02:53:04.419867Z","iopub.status.idle":"2022-02-24T02:53:04.870874Z","shell.execute_reply.started":"2022-02-24T02:53:04.419818Z","shell.execute_reply":"2022-02-24T02:53:04.869893Z"}}
 plotHist(history)
 
-y_pred = model.predict(x_test)
+# %% [code] {"execution":{"iopub.status.busy":"2022-02-24T02:53:06.743848Z","iopub.execute_input":"2022-02-24T02:53:06.744502Z","iopub.status.idle":"2022-02-24T02:53:24.018249Z","shell.execute_reply.started":"2022-02-24T02:53:06.744459Z","shell.execute_reply":"2022-02-24T02:53:24.017242Z"}}
+y_pred = model.predict(test)
 y_pred_max = np.argmax(y_pred, axis=1)
-cf_matrix = confusion_matrix(y_true=y_test, y_pred=y_pred_max)
-ax = sns.heatmap(cf_matrix, annot=True, cmap='Blues')
 
-ax.set_title('Brain Tumor CT Scans Confusion Matrix \n\n')
+labels = (train.class_indices)
+labels = dict((v,k) for k,v in labels.items())
+y_pred = [labels[k] for k in y_pred_max]
+
+y_test = testDf.targets
+
+# %% [code] {"execution":{"iopub.status.busy":"2022-02-24T02:53:51.630284Z","iopub.execute_input":"2022-02-24T02:53:51.631205Z","iopub.status.idle":"2022-02-24T02:53:52.206877Z","shell.execute_reply.started":"2022-02-24T02:53:51.63116Z","shell.execute_reply":"2022-02-24T02:53:52.205751Z"}}
+cf_matrix = confusion_matrix(y_true=y_test, y_pred=y_pred)
+ax = sns.heatmap(cf_matrix, annot=True, cmap='Blues', fmt='g')
+print("Accuracy of the Model:",accuracy_score(y_test, y_pred)*100,"%")
+print(classification_report(y_test, y_pred))
+
+ax.set_title('Brain Tumor CT Scans Confusion Matrix \n\n');
 ax.set_xlabel('\nPredicted Tumor Present')
-ax.set_ylabel('Actual Tumor Present ')
+ax.set_ylabel('Actual Tumor Present ');
 
 ax.xaxis.set_ticklabels(['False','True'])
 ax.yaxis.set_ticklabels(['False','True'])
 
 plt.show()
 
+# %% [code]
